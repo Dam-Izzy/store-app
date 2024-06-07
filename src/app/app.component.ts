3466bc54979ee0394a80f59a6545729bafbe2cf5
Dam Izzy <damizzy@MacBook-Air-de-Dam.local>
2024-06-07 12:52:38 -0600

    Agregué cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/poke-table/poke-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './poke-table.component.html',
+  styleUrl: './poke-table.component.css'
+})
+export class PokeTableComponent {
+
+}
